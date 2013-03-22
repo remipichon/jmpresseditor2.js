5c1440d704d275a0562d97a6f527f31163214d0f
Claire Zuliani <clairezuliani@gmail.com>
2013-03-22 14:04:47 +0100

    test

--- js/creation.js
+++ js/creation.js
@@ -1,13 +1,13 @@
 
 /*
 tout ce qui est dans la fonction $( function() {
-concerne les elements crée en statique
+concerne les elements cr?e en statique
 pour que des fonctions soient prises en charge par des elements
-crée dynamiquement, il faut les lui donner lors de sa création, 
+cr?e dynamiquement, il faut les lui donner lors de sa cr?ation, 
 ex : le draggable et le doubleclick
 
 il faut delemiter une zone de canvas en mode creation en limityant le draggable
-ensuite calculer en proportion les coords à foutre pour le impress
+ensuite calculer en proportion les coords ? foutre pour le impress
 
 trouver un moyen astucieux pour gerer les elements au sein de la slide
  */
@@ -15,8 +15,8 @@ trouver un moyen astucieux pour gerer les elements au sein de la slide
 $( function () {
     alert("Bonsoir !")
     alert("en double cliquant sur UN square ou UN texte  puis sur UNE slide tu peux les grouper via le bouton 'group' ");
-    alert("au sein de chaque slide, le petit cadre bleu indique le positionnement de la slide dans l'espace et le grand cadre noir est pour placé les elements (tu ne peux le faire qu'avec le carré) au sein de la slide");
-    alert("ensuite, clique sur 'creer diapo' pour apprecier la presentation. Backspace pour revenir à l'édition et modifier ta présentaion");
+    alert("au sein de chaque slide, le petit cadre bleu indique le positionnement de la slide dans l'espace et le grand cadre noir est pour plac? les elements (tu ne peux le faire qu'avec le carr?) au sein de la slide");
+    alert("ensuite, clique sur 'creer diapo' pour apprecier la presentation. Backspace pour revenir ? l'?dition et modifier ta pr?sentaion");
     alert("malheureusement, pour le moment tu ne peux pas cliquer de nouveau sur 'creer diapo', il faut refresh la page pour recommencer");
     
     
@@ -24,6 +24,7 @@ $( function () {
     var cptSlide = 0;
     var cptElement = 0;
    
+    $("slider").slider();
     
     
     //ajout carre draggable avec son indice et double click pour le groupage
@@ -103,7 +104,7 @@ $( function () {
             var Left = grp.left*10;
             var Top = grp.top*10;
            
-            //modification de la slide pour qu'elle soit géré par impress
+            //modification de la slide pour qu'elle soit g?r? par impress
             var slide = $("#"+i+"").clone();
             slide.removeClass();
             slide.removeAttr('style');
@@ -116,25 +117,25 @@ $( function () {
         
         $(".zonePouet").remove();
         //
-        //suppression des scripts relatif à la zone création et a boostrap
-        //finalement je garde car c'est dans ce div que seront stocké les css des formes et tout
+        //suppression des scripts relatif ? la zone cr?ation et a boostrap
+        //finalement je garde car c'est dans ce div que seront stock? les css des formes et tout
         //$("#scriptBootstrap").empty();
         
-        //chargement des scriptis relatif à impress
+        //chargement des scriptis relatif ? impress
         //ce css permet de faire l'echange entre les display des id=impress et id=editot
         $('#scriptImpress').append( '<link id="impress-demo" href="css/impress-demo.css" rel="stylesheet" />');
         //$('#impress').append(' <a class="btn btn-large" id="Edit" href="#">Edit<i class="icon-edit"></i> </a> ');
-        //le bouton de retour à l'édition est présent dès le début mais en display:none car dans la div impress
+        //le bouton de retour ? l'?dition est pr?sent d?s le d?but mais en display:none car dans la div impress
        
         
-        impress().init();
+        $('impress').jmpress();
         alert("c'est parti pour impress");
     });
     
     //*
     $(document).keydown( function(key) {
         if(key.keyCode === 8){ //backspace 
-            $("#impress-demo").remove();
+            deinit();
             //$("#impress").children().empty(); //purge de id=impress
             //$("body").append('<div id="impress"> </div>');
        
@@ -143,14 +144,14 @@ $( function () {
     
     /*
     $('#Edit').click( function() {
-        //charcher les scripts pour le mode de création
+        //charcher les scripts pour le mode de cr?ation
         alert("mode edition");
         $("#impress-demo").remove();      
     });
     */
     
     $('#grouper').click( function() {        
-        //le calcul des coordonnées ne gère pas le fait de grouper plusieurs elements d'un coup
+        //le calcul des coordonn?es ne g?re pas le fait de grouper plusieurs elements d'un coup
         var grp = $('.groupageG');
         var elmt = $('.groupage');
         
