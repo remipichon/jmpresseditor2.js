597a11873bb3a03807c523b8169caf50e017954e
Kikivousaime <pichon.remi.pr@gmail.com>
2013-05-01 21:00:51 +0200

    NZD posData replaced by $(element).data( ) (each slides contains its attributes once there is a mousedown and grandMotherSlide contains event's position)

--- js/navigable-zoombale-draggable.js
+++ js/navigable-zoombale-draggable.js
@@ -96,18 +96,8 @@ function getRealCoord(element, $slideArea) {        //semble bien fonctionner
  * ====================================================================================== */
 
 
-/* permet de stocker des positions initiales, init lors d'un mousedown */
-var posData = {
-    //pour l'event
-    x: 0,
-    y: 0,
-    //pour l'objet (stocke les data-x/y/z ou bien les data-rotate-x/y/z
-    xObj: 0,
-    yObj: 0,
-    zObj: 0,
-    //pour l'objet, permet de stocker le scale
-    scale: 0
-};
+
+
 
 /* 
  * obtenir la distance depuis un evenement (marqué par l'initialisation du posData
@@ -118,8 +108,8 @@ function getDistanceMouseMove(event) {
 
     //recupération de déplacement de la souris
     var distance = {//element différentiel reel
-        x: event.pageX - posData.x,
-        y: event.pageY - posData.y
+        x: event.pageX - $("#slideArea").data('event').pos.x,
+        y: event.pageY - $("#slideArea").data('event').pos.y
     };
     return distance;
 }
@@ -255,12 +245,13 @@ function moveZ(event, $objet) {
     var distance = getDistanceMouseMove(event);
     //console.log("distance " + distance.x + "  " + distance.y);
     var tolerance = 40;
+    var ratio = 100;
     /* explication du bout de code ci dessous :
      * idem que pour rotate, sauf que cette fois ci on ne permet pas de distance en diagonale (ceci afin de permettre d'assigner
      * un autre comportement à distance.x)
      */
     var newZ = ((Math.abs(distance.y) - tolerance) >= 0 && Math.abs(distance.x) < tolerance ?
-            posData.zObj - (distance.y - tolerance * Math.abs(distance.y) / distance.y) * 100
+            $objet.data('pos').z - (distance.y - tolerance * Math.abs(distance.y) / distance.y) * ratio
             : $objet.attr("data-z"));
 
 
@@ -285,8 +276,9 @@ function resize(event, $objet) {
     var distance = getDistanceMouseMove(event);
 
     var tolerance = 40;
+    var ratio = 1 / 15;
     var newScale = ((Math.abs(distance.x) - tolerance) >= 0 && Math.abs(distance.y) < tolerance ?
-            posData.scale - (distance.x - tolerance * Math.abs(distance.x) / distance.x) / 10
+            $objet.data("scale") - (distance.x - tolerance * Math.abs(distance.x) / distance.x) * ratio
             : $objet.attr("data-scale"));
 
 
@@ -308,20 +300,21 @@ function resize(event, $objet) {
  * @param {type} $objet
  */
 function rotate(event, $objet) {
-    var $slideMother = $("#slideArea >");
-    var $slideGrandMother = $("#slideArea");
+
 
     var distance = getDistanceMouseMove(event);
 
     //tolerance de 20 pixel de deplacement reel, peut etre faudrait il connecter cette tolerance à la taille de l'écran -> ergonomie
     var tolerance = 20;
+    var ratioX = 1;
+    var ratioY = ratioX;
     var rotate = {
         /* explication du bout de code ci dessous :
          * condition ternaire, si la distance est inférieur à la tolérance, aucune rotation
          * sinon on soustrait à la distance la tolérance doté du signe de la distance (abs(x)/x = signe de x)
          */
-        x: ((Math.abs(distance.y) - tolerance) >= 0 ? distance.y - tolerance * Math.abs(distance.y) / distance.y : 0),
-        y: ((Math.abs(distance.x) - tolerance) >= 0 ? distance.x - tolerance * Math.abs(distance.x) / distance.x : 0)
+        x: ((Math.abs(distance.y) - tolerance) >= 0 ? $objet.data('rotate').x - (distance.y - tolerance * Math.abs(distance.y) / distance.y) * ratioX : $objet.data('rotate').x),
+        y: ((Math.abs(distance.x) - tolerance) >= 0 ? $objet.data('rotate').y - (distance.x - tolerance * Math.abs(distance.x) / distance.x) * ratioY : $objet.data('rotate').y)
     };
 
 
@@ -359,7 +352,7 @@ function rotateZ(event, $objet) {
          * ceci car le rotateZ n'est commandé que par le deplacement en X (le depalcement en Y est dispo pour autre chose
          */
         z: ((Math.abs(distance.x) - tolerance) >= 0 && Math.abs(distance.y) < tolerance ?
-                posData.zObj - (distance.x - tolerance * Math.abs(distance.x) / distance.x) / 10
+                $objet.data('rotate').z - (distance.x - tolerance * Math.abs(distance.x) / distance.x) / 10
                 : $objet.attr("data-rotate-z"))
     };
 
@@ -458,31 +451,44 @@ jQuery.fn.draggableKiki = function() {
 //        $(this.off('.rotate');
 //        $(this.off('.rotateZ');
 
+    //init du posData qui permet de stocker les caractéristiques de l'objet lors du mousedown
 
+    var $this = $(this); // .data ne fait pas traverser this
+    $(this).data('pos', {
+        x: $this.attr('data-x'),
+        y: $this.attr('data-y'),
+        z: $this.attr('data-z')});
+    $(this).data('rotate', {
+        x: $this.attr('data-rotate-x'),
+        y: $this.attr('data-rotate-y'),
+        z: $this.attr('data-rotate-z')});
+    $(this).data('scale', $this.attr('data-scale'));
     /*
      * gestion des long press click
      * left : draggable Z
      * right : rotate Z
      */
     $(this).on('mousedown.longclick', function(event) {
-        //init du posData qui permet de stocker les caractéristiques de l'objet lors du mousedown
-        posData.x = event.pageX;
-        posData.y = event.pageY;
-        posData.xObj = $(this).attr('data-x');
-        posData.yObj = $(this).attr('data-y');
-        posData.zObj = $(this).attr('data-z');
-        posData.scale = $(this).attr('data-scale');
+
+        $("#slideArea").data('event', {
+            pos: {
+                x: event.pageX,
+                y: event.pageY
+            }
+        });
+
+
+
 
 
 
         //event.stopImmediatePropagation();  //il n'en faut pas ici car il n'en faut qu'un seul par type de trigger
-        var $this = $(this); // .data ne fait pas traverser this
+
 
         if (event.which === 1) {//longpress left
             $(this).data('checkdown', setTimeout(function() {
                 console.log('long right press sur slide');
                 $this.addClass("longclick");
-                $this.addClass('alt');
                 //mettre ici les trigger à annuler pour un mousemove sur le meme element
                 //par exemple, ici on supprime le trigger mousemove.simpleclick ddu draggableKiki
                 //$this.off(".simpleclick");
@@ -496,8 +502,6 @@ jQuery.fn.draggableKiki = function() {
 
         if (event.which === 3) {//longpress right
             $(this).data('checkdown', setTimeout(function() {
-                posData.zObj = $this.attr('data-rotate-z');
-                console.log('long left press sur slide');
                 $this.addClass("longclick");
                 //mettre ici les trigger à annuler pour un mousemove sur le meme element
                 //par exemple, ici on supprime le trigger mousemove.simpleclick ddu draggableKiki
@@ -524,8 +528,13 @@ jQuery.fn.draggableKiki = function() {
      * rotate X et Y (right)
      */
     $(this).on("mousedown.simpleclick", function(event) {
-        posData.x = event.pageX;
-        posData.y = event.pageY;
+
+        $("#slideArea").data('event', {
+            pos: {
+                x: event.pageX,
+                y: event.pageY
+            }
+        });
 
 
         event.stopImmediatePropagation();           //empeche l'event de bubble jusqu'à la slide mère et le document, ainsi pas de conflits avec le navigable
@@ -609,7 +618,7 @@ function setTrans3D(dico) {
     var transform = "translate(" + dico.translate[0] + "%, " + dico.translate[1] + "%) scaleX(" + dico.scaleX + ") scaleY(" + dico.scaleY + ") scaleZ(" + dico.scaleZ + ") translate3d(0px,0px,0px) scaleX(1) scaleY(1) scaleZ(1) rotateZ(" + dico.rotateZ + "deg) rotateY(" + dico.rotateY + "deg) rotateX(" + dico.rotateX + "deg) translate3d(" + dico.translate3d[0] + "px," + dico.translate3d[1] + "px, " + dico.translate3d[2] + "px)";
 
     $("#slideArea>div").css({'transform': transform});
-    console.log("setter " + transform);
+    //console.log("setter " + transform);
 
 
 }
@@ -621,18 +630,18 @@ function setTrans3D(dico) {
  * 
  * ====================================================================================== */
 $(document).on('mousedown', function(event) {           //le fucking probleme avec cette methode c'est que le mousemove et mouseup sont absorbé par une autre slide si notre draggable passe dessous
+    //init du posData qui permet de stocker les caractéristiques de l'objet lors du mousedown
+    $("#slideArea").data('event', {
+        pos: {
+            x: event.pageX,
+            y: event.pageY
+        }
+    });
 
-    var $slideMother = $("#slideArea >");
+
 
     if (event.which === 1) {
-        //zone ou sont stocker les slides 
-        var $slideMother = $("#slideArea >");
-        var $slideGrandMother = $("#slideArea");
-        //initialisation du posData
-        posData.x = event.pageX;
-        posData.y = event.pageY;
-
-        $(this).on('mousemove.navigable', function(event) {
+        $(this).on('mousemove.moveView', function(event) {
 
             //recupération des attributs de positionnement de la view
             var transform = getTrans3D();
@@ -642,8 +651,8 @@ $(document).on('mousedown', function(event) {           //le fucking probleme av
 
             //recupération de déplacement de la souris
             var dReal = {//element différentiel reel
-                x: event.pageX - posData.x,
-                y: event.pageY - posData.y
+                x: event.pageX - $("#slideArea").data('event').pos.x,
+                y: event.pageY - $("#slideArea").data('event').pos.y
             };
 
             //calcul du déplacement dans le monde des slides
@@ -723,7 +732,7 @@ $(document).on('mousedown', function(event) {           //le fucking probleme av
 
 
 
-          
+
 
 
             transform.translate3d[0] = parseInt(transform.translate3d[0] - dVirtuel.x);
@@ -732,17 +741,50 @@ $(document).on('mousedown', function(event) {           //le fucking probleme av
 
 
             //mise à jour de l'ancienne position de souris, pour avoir l'element différent de déplacement reel
-            posData.x = event.pageX;
-            posData.y = event.pageY;
+            $("#slideArea").data('event').pos.x = event.pageX;
+            $("#slideArea").data('event').pos.y = event.pageY;
+
 
             //deplacement de la zone de vue
             setTrans3D(transform);
         });
+    }
 
-        $(this).on("mouseup", function() {
-            $(this).off(".navigable");
+
+    if (event.which === 3) {
+        $(this).on('mousemove.rotateView', function(event) {
+
+            //recupération des attributs de positionnement de la view
+            var transform = getTrans3D();
+            var alpha = transform.rotateX;
+            var beta = transform.rotateY;
+            var gamma = transform.rotateZ;
+
+           /***********************
+            * 
+            * insert code here
+            * 
+            ************************/
+
+
+
+            //mise à jour de l'ancienne position de souris, pour avoir l'element différent de déplacement reel
+            $("#slideArea").data('event').pos.x = event.pageX;
+            $("#slideArea").data('event').pos.y = event.pageY;
+
+
+            //deplacement de la zone de vue
+            setTrans3D(transform);
+
         });
     }
+
+
+    $(this).on("mouseup", function() {
+        $(this).off(".moveView");
+        $(this).off(".rotateView");
+
+    });
 
 });
 
